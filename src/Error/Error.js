b27155be5f01b803c5d0f19042bf7a6ed8b3ebfb
bannadas <bannadaso39@gmail.com>
2021-10-05 14:15:24 +0600

    error msg changed

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Error = () => {
     return (
         <div>
-            <h1 style={{color:'red'}}>Can't find error 404 !!!</h1>
+            <h1 style={{color:'red'}}>Website not found!!
+            ERROR 404</h1>
         </div>
     );
 };
